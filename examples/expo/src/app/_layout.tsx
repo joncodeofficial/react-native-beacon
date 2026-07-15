00a049ff93c45e9ae048c69a7eac9521d73f9ad4
jonpena <gran_200014@hotmail.com>
2026-07-15 23:53:48 +0200

    refactor: remove AnimatedSplashOverlay and related components

--- examples/expo/src/app/_layout.tsx
+++ examples/expo/src/app/_layout.tsx
@@ -1,13 +1,10 @@
 import { DarkTheme, DefaultTheme, Slot, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
       <Slot />
     </ThemeProvider>
   );
